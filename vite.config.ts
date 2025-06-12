7ef2a4efb497b0fb55369771c43dde1825fdf3b7
Your Nametanbhir9589git config --global user.email tanbhir.ahmed.tonu@gmail.comgit config --global user.name Your <tanbhir.ahmed.tonu@gmail.com>
2025-06-12 11:21:33 +0600

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Protflio-code/',
   server: {
     host: "::",
     port: 8080,
